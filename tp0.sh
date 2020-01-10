9bcbe78d8eb45d6f022e50b4d3da9f2cfe5ce5e7
Sullivan <Sullivan@NANTES-0169.sii.fr>
2020-01-10 14:09:50 +0100

    ajout tp0.sh

---
+++ tp0.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo -----------------------------------------------------------
+echo Lancement du script tp0.sh  
+echo -----------------------------------------------------------
+
+git clone https://github.com/SII-Codelab-Chaos/Codelab-Chaos-TP.git
+
+git clone https://github.com/SII-Codelab-Chaos/Gatling-Load-Tests-FuSIIon.git
+
+git clone https://github.com/SII-Codelab-Chaos/kubernetes-yaml.git
+
+git clone https://github.com/SII-Codelab-Chaos/fusiion-gestion-competences.git
+
+docker pull fusiion/sii-codelab-chaos-statistiques
+
+docker pull fusiion/sii-codelab-chaos-notification
+
+docker pull fusiion/sii-codelab-chaos-gestion-competences
+
+docker pull fusiion/sii-codelab-chaos-gestion-collaborateurs
+
+docker pull fusiion/sii-codelab-chaos-gestion-clients
+
+docker pull fusiion/sii-codelab-chaos-gaming
+
+docker pull fusiion/sii-codelab-chaos-authentification
+
+docker pull fusiion/sii-codelab-chaos-front
