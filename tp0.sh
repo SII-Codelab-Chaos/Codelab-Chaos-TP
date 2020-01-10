56544b4b81a930f2835c0dcc1127f34010ede7fb
Sullivan <Sullivan@NANTES-0169.sii.fr>
2020-01-10 14:15:03 +0100

    ajout tp0.sh

--- tp0.sh
+++ tp0.sh
@@ -27,3 +27,9 @@ docker pull fusiion/sii-codelab-chaos-gaming
 docker pull fusiion/sii-codelab-chaos-authentification
 
 docker pull fusiion/sii-codelab-chaos-front
+
+docker pull bitnami/mongodb:3.6.6
+
+docker pull neo4j:3.1
+
+docker pull rabbitmq:management
