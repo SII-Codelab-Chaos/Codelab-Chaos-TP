bdfe3187735b8e18fb60fe669394cef3e8dcf9d5
Sullivan <Sullivan@NANTES-0169.sii.fr>
2020-01-11 01:41:13 +0100

    refatoring + ajout du readinessProbe

--- TP3-kubernetes-yaml/delete.sh
+++ TP3-kubernetes-yaml/delete.sh
@@ -9,19 +9,17 @@ kubectl config set-context --current --namespace=default
 # pas de param
 if [ $# == 1 ] ; then
     if [ $1 == "all" ] ; then
-        kubectl delete -f 'app-fusiion\all\' | echo 1a/7 - delete all deployments
+        kubectl delete -f 'app\all\' | echo 1a/6 - delete all deployments
     fi
 fi    
-kubectl delete -f 'app-fusiion\deployments-fusiion\' | echo 1/7 - delete deployments
+kubectl delete -f 'deployments-fusiion\' | echo 1/6 - delete deployments
 
-kubectl delete -f 'app-fusiion\default\' | echo 2/7 - delete default
+kubectl delete -f 'app\default\' | echo 2/6 - delete default
 
-kubectl delete -f 'app-fusiion\statefulsets-fusiion\' | echo 3/7 - delete statefulsets
+kubectl delete -f 'app\statefulsets-fusiion\' | echo 3/6 - delete statefulsets
 
-kubectl delete -f 'app-fusiion\secrets-config\' | echo 4/7 - delete secrets
+kubectl delete -f 'app\secrets-config\' | echo 4/6 - delete secrets
 
-kubectl delete -f 'fusiion-rbac.yaml' | echo 5/7 - delete rbac 
+kubectl delete -f 'fusiion-rbac.yaml' | echo 5/6 - delete rbac 
 
-kubectl delete -f 'kubernetes-metrics-server\'  | echo 6/7 - delete metrics
-
-kubectl delete -f 'fusiion-namespace.yaml' | echo -e '7/7 - delete namespace \n-----------------------------------------------------------  \nALL DELETE \n-----------------------------------------------------------'
+kubectl delete -f 'fusiion-namespace.yaml' | echo -e '6/6 - delete namespace \n-----------------------------------------------------------  \nALL DELETE \n-----------------------------------------------------------'
--- TP3-kubernetes-yaml/run.sh
+++ TP3-kubernetes-yaml/run.sh
@@ -4,26 +4,24 @@ echo -----------------------------------------------------------
 echo Lancement du script run.sh  
 echo -----------------------------------------------------------
 
-kubectl create -f 'fusiion-namespace.yaml' | echo 1/7 - create namespace Done
+kubectl create -f 'fusiion-namespace.yaml' | echo 1/6 - create namespace Done
 
-kubectl create -f 'fusiion-rbac.yaml' | echo 2/7 - create rbac Done
+kubectl create -f 'fusiion-rbac.yaml' | echo 2/6 - create rbac Done
 
-kubectl create -f 'app-fusiion\secrets-config\' | echo 3/7 - create secrets Done
+kubectl create -f 'app\secrets-config\' | echo 3/6 - create secrets Done
 
-kubectl create -f 'app-fusiion\statefulsets-fusiion\' | echo 4/7 - create statefulsets Done
-
-kubectl create -f 'app-fusiion\default\' | echo 5/7 - create default Done
-
-kubectl create -f 'kubernetes-metrics-server\'  | echo 6/7 - create metrics Done
+kubectl create -f 'app\statefulsets-fusiion\' | echo 4/6 - create statefulsets Done
+    
+kubectl create -f 'app\default\' | echo 5/6 - create default Done
 
 # pas de param
 if [ $# == 1 ] ; then
     if [ $1 == "all" ] ; then
-        kubectl create -f 'app-fusiion\all\' | echo 7a/7 - ALL deployments Done
+        kubectl create -f 'app\all\' | echo 6a/6 - ALL deployments Done
     fi
 fi  
 
-kubectl create -f 'app-fusiion\deployments-fusiion\' | echo -e '7/7 - deployments Done \n-----------------------------------------------------------  \n ALL DONE \n-----------------------------------------------------------'
+kubectl create -f 'deployments-fusiion\' | echo -e '6/6 - deployments Done \n-----------------------------------------------------------  \n ALL DONE \n-----------------------------------------------------------'
 
 kubectl config set-context --current --namespace=fusiion
 
